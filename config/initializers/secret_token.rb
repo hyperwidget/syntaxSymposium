dea66dab931bc6d5a1da4b1e711ee7706557f4bb
Hunter Jansen <rawkamatic@gmail.com>
2012-07-23 21:45:22 -0400

    layout the style

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SyntaxSymposium::Application.config.secret_token = '0eadbc52fd6ee5871f6adc0c4241c994f3e60c50344a4e7c65ae84c5ea0ea54a9e0259423b78bd38a1766421d2ba99cd025057ff5cfe1dc7b683aa70959e2af8'
